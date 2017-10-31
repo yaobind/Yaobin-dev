4f35c1c2659bacb68676a0bcedde13ba84ffecdc
yaobind <yaobin.dong@gmail.com>
2017-10-30 23:08:27 -0400

    better float to english

--- js/floatToEnglish.js
+++ js/floatToEnglish.js
@@ -1,73 +1,62 @@
 var singles = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
-var Tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
-var TenPlus = ["", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+var Tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];//move Ten to TenPlus array
+var TenPlus = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
 var unitStr = ["Billion", "Million", "Thousand"];
-var unit = [1000000000, 1000000, 1000];
+var unit = [1e9, 1e6, 1000];//use scientific notition
 var numberToWords = function(num) {
+    //ask for clarification null undefined 
     if (num === 0) {
         return "Zero";
     }
     
-    if (num < 0 || num > 1000000000000) { //!!!
+    if (num < 0 || num >= 1e12) {
         return "Invalid number";
     }
     
+    var decimal = num - Math.floor(num);
+    num = Math.floor(num);//!!!
     var result = [];
-    var decimal = num - Math.floor(num); //!!!
-    num = Math.floor(num);
     
-    // note: for (x in person) only for obj properties
-    for (var i = 0; i < 3; i++) {
-        if (num >= unit[i]) { // !!!
-            var curt = ~~(num / unit[i]);
-            under1000(curt, result);
-            result.push(unitStr[i]);
+    unit.forEach(function(unitVal, index) {
+        if (num < unitVal) {
+            return;
         }
+        
+        under1000(Math.floor(num / unitVal), result);
+        result.push(unitStr[index]);
+        num %= unit[index];
+    });
 
-        num = num % unit[i];
+    if (num > 0) {
+        under1000(num, result);
     }
-    
-    under1000(num, result);
     
     if (decimal > 0) {
-        result.push(~~(decimal * 100) + '/100'); // !!! 
+        result.push( Math.floor(decimal * 100) + "/100");
     }
     
-    return format(result);
-};
-
-function format(list) {
-    if (!list || list.length == 0) {
-        return "";
-    }
-    
-    var result = list[0];
-    for (var i = 1; i < list.length; i++) {
-        result += ' ' + list[i];
-    }
-    
-    return result;
+    return result.join(" ");
 }
 
-function under1000(num, list) {
-    if (num >= 100) {
-        list.push(singles[~~(num / 100)])
-        list.push("Hundred");
-        num = num % 100;
+function under1000(num, result) {
+    if (num >= 100) {//!!! 
+        result.push(singles[Math.floor(num / 100)]);
+        result.push("Hundred");
+        num %= 100;
     }
     
-    if (num < 20 && num > 10) {
-        list.push(TenPlus[num - 10]);
-        return;
+    if (num >= 20) {
+        result.push(Tens[Math.floor(num / 10)]);
+        num %= 10;
     }
     
     if (num >= 10) {
-        list.push(Tens[~~(num / 10)]);
-        num = num % 10;
+        result.push(TenPlus[num - 10]);
+        return;
     }
     
     if (num > 0) {
-        list.push(singles[num]);
+        result.push(singles[num]);
     }
 }
 
@@ -79,6 +68,11 @@ console.log('|' + numberToWords(123.4451234123541234) + '|');
 console.log('|' + numberToWords(1234567890.12345) + '|');
 console.log('|' + numberToWords(0.267) + '|');
 console.log('|' + numberToWords(0) + '|');
+console.log('|' + numberToWords(1) + '|');
+console.log('|' + numberToWords(10) + '|');
+console.log('|' + numberToWords(11) + '|');
+console.log('|' + numberToWords(20) + '|');
+console.log('|' + numberToWords(100) + '|');
 
 //invalid
 console.log('|' + numberToWords(-1) + '|');
